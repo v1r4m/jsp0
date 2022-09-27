e2782baa98a82d6a7e9fa5ea4cb0bfa7e2dea797
v1r4m <choyoung7789@gmail.com>
2022-09-27 15:56:16 +0900

    connect mariaDB, but not in a well-developed secured method

--- demo/src/main/java/com/example/demo/BookController.java
+++ demo/src/main/java/com/example/demo/BookController.java
@@ -4,15 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BookController {
-    // @GetMapping("/create")
-    // public String test() {
-    //     return "test!!";
-    // }
     @RequestMapping(value="/create", method=RequestMethod.GET)
     public ModelAndView create(){
         return new ModelAndView("book/create");
