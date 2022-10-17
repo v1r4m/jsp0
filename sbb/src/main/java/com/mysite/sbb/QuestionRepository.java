12162ae0ae8351af65cc4ea8d4b58ce3f40859e3
v1r4m <choyoung7789@gmail.com>
2022-10-17 16:08:08 +0900

    2-05 finish

--- sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,7 +1,11 @@
 package com.mysite.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Question findBySubject(String subject); //상속을 받아도 선언해줘야 사용할 수 있단
+    Question findBySubject(String subject); //상속을 받아도 선언해줘야 사용할 수 있다
+    Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String subject);
+    //save get 정도는 걍 되는듯!?
 }
--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,12 +3,15 @@ package com.mysite.sbb;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.List;
 
 
 @SpringBootTest
@@ -18,9 +21,20 @@ class SbbApplicationTests {
 	//테스트 코드에서는 @Autowired를 써야하고 실제로는 생성자를 통한 객체 생성이 권장된다.(순환참조이슈)
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Transactional
     @Test
     void testJpa() {        
-        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-		assertEquals(1, q.getId());
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList(); //automated by lombok
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+
     }
 }
