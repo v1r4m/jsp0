95573b9ca3dfadfb9d643195799ca6f2867ac59f
v1r4m <choyoung7789@gmail.com>
2022-10-17 14:03:50 +0900

    findBySubject

--- sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -3,5 +3,5 @@ package com.mysite.sbb;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
+    Question findBySubject(String subject); //상속을 받아도 선언해줘야 사용할 수 있단
 }
--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,29 +2,25 @@ package com.mysite.sbb;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 
 @SpringBootTest
 class SbbApplicationTests {
 
-    @Autowired
+    @Autowired //생성자를 대체 가능한 spring의 annotation.
+	//테스트 코드에서는 @Autowired를 써야하고 실제로는 생성자를 통한 객체 생성이 권장된다.(순환참조이슈)
     private QuestionRepository questionRepository;
 
     @Test
     void testJpa() {        
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);  // 두번째 질문 저장
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
     }
 }
